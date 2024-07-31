bb1366a39e03e356c01a7d69b8ce175418331bcf
semanticist21 <semanticist0@gmail.com>
2024-07-31 11:10:04 +0900

    init

--- apps/react-monorepo-2/rspack.config.js
+++ apps/react-monorepo-2/rspack.config.js
@@ -1,5 +1,17 @@
 const { composePlugins, withNx, withReact } = require('@nx/rspack');
 
 module.exports = composePlugins(withNx(), withReact(), (config) => {
-  return config;
+  return {
+    ...config,
+    module: {
+      rules: [
+        ...config.module.rules,
+        {
+          test: /\.css$/,
+          use: ['postcss-loader'],
+          type: 'css',
+        },
+      ],
+    },
+  };
 });
